adf9a12585b399efff216ab377832ed924005b6d
Raahul <sridhardscv@gmail.com>
2023-07-07 08:40:51 +0530

    file clean doit clean

---
+++ dodo1.py
@@ -0,0 +1,13 @@
+def task_echo_to_cave():
+    return {
+        "actions": [
+             "echo pssst > cave.txt",
+             "echo holla >> cave.txt",
+             "echo olla >> cave.txt",
+             "echo lla >> cave.txt",
+             "echo la >> cave.txt",
+             "echo a >> cave.txt",
+        ]
+    }
+    # doit list
+    # doit run echo_to_cave 
